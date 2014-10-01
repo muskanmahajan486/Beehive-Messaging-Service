de41036227d9fa49e7366367ac900420a0420f09
Eric Bariaux <eric@openremote.org>
2014-10-01 14:11:53 +0200

    First implementation of sending e-mail using standard JavaMail API

--- src/main/java/org/openremote/messaging/EMailMessageResource.java
+++ src/main/java/org/openremote/messaging/EMailMessageResource.java
@@ -20,6 +20,18 @@
  */
 package org.openremote.messaging;
 
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletConfig;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -42,12 +54,60 @@ public class EMailMessageResource
   @POST
   @Consumes("application/json")
   @Produces("application/json")
-  public Response sendEMailMessage(@Context ServletConfig config,
-                                 @Context SecurityContext sc,
-                                 EMailMessage message)
+  public Response sendEMailMessage(@Context final ServletConfig config,
+      @Context SecurityContext sc, EMailMessage message)
   {
-    // TODO: implement e-mail sending using standard Java mail API
+    // TODO: validate payload and return appropriate error messages
     
+    Properties props = System.getProperties();
+    props.put("mail.smtp.host", config.getInitParameter("mail.smtp.host"));
+    props.put("mail.smtp.ssl.enable", "true");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+
+    // Get a Session object
+    Session session = Session.getInstance(props, new Authenticator()
+    {
+      @Override
+      protected PasswordAuthentication getPasswordAuthentication()
+      {
+        return new PasswordAuthentication(
+            config.getInitParameter("mail.smtp.user"),
+            config.getInitParameter("mail.smtp.password"));
+      }
+    });
+
+    session.setDebug(true);
+
+    Message msg = new MimeMessage(session);
+    try
+    {
+      msg.setFrom(new InternetAddress(config.getInitParameter("mail.smtp.from")));
+      for (String recipient : message.getRecipients())
+      {
+        msg.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+      }
+
+      msg.setSubject(message.getSubject());
+      msg.setText(message.getMessage());
+
+      msg.setSentDate(new Date());
+
+      Transport.send(msg);
+    } catch (AddressException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (MessagingException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    System.out.println("\nMail was sent successfully.");
+
+    // TODO: implement e-mail sending using standard Java mail API
+
     return Response.ok().build();
   }
 
